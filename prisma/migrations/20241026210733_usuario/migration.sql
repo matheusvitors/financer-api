7d8aeb378b12ebab8b26394f0ea2595e3951706a
Matheus Vitor Sousa Almeida <matheusvitor.dev@gmail.com>
2024-10-26 19:04:06 -0300

    feat: usuarios e autenticação e autorização

---
+++ prisma/migrations/20241026210733_usuario/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `Usuario` (
+    `id` VARCHAR(191) NOT NULL,
+    `nome` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Usuario_id_key`(`id`),
+    UNIQUE INDEX `Usuario_username_key`(`username`),
+    UNIQUE INDEX `Usuario_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ src/application/controllers/authentication/authentication-controller.ts
@@ -0,0 +1,37 @@
+import { Repository, ResponseData } from "@/application/interfaces";
+import { Usuario } from "@/core/models";
+import { verifyHash } from "@/infra/adapters/encryption";
+import { jwt } from "@/infra/adapters/jwt";
+import { notFound, serverError, success, unauthorized } from "@/infra/adapters/response-wrapper";
+
+interface AuthenticationControllerParams {
+	repository: Repository<Usuario>;
+	username: string;
+	password: string;
+}
+
+export const authenticationController = async (params: AuthenticationControllerParams): Promise<ResponseData> => {
+
+	try {
+
+		const { repository, username, password } = params;
+		const usuario = await repository.find!('username', username);
+
+		if(!usuario) {
+			return notFound('Usuário não encontrado.');
+		}
+
+		const isCorrectPassword = await verifyHash(password, usuario.password);
+
+		if(!isCorrectPassword) {
+			return unauthorized('Senha incorreta.')
+		}
+
+		const token = jwt.encode({payload: {auth: true, id: usuario.id}});
+		return success({token});
+	} catch (error) {
+		console.error(error);
+		return serverError(error)
+	}
+
+}
